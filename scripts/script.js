3c882c69e103621c315210818eaf37b671ef6fc8
jpullavelecela32 <112657307+jpullavelecela32@users.noreply.github.com>
2023-04-04 18:12:02 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,190 @@
+let myData = [{"DATE":"7/1/14","Used cars percentage increase":"0%","New cars pct increase":"0%","Avg.Hourly earnings":"0%","Real Median household income":'-2%'},
+{"DATE":"8/1/14","Used cars percentage increase":"0%","New cars pct increase":"0%","Avg.Hourly earnings":"0%","Real Median household income":null},
+{"DATE":"9/1/14","Used cars percentage increase":"1%","New cars pct increase":"0%","Avg.Hourly earnings":"0%","Real Median household income":null},
+{"DATE":"10/1/14","Used cars percentage increase":"0%","New cars pct increase":"1%","Avg.Hourly earnings":"0%","Real Median household income":null},
+{"DATE":"11/1/14","Used cars percentage increase":"-1%","New cars pct increase":"1%","Avg.Hourly earnings":"1%","Real Median household income":null},
+{"DATE":"12/1/14","Used cars percentage increase":"-2%","New cars pct increase":"1%","Avg.Hourly earnings":"1%","Real Median household income":null},
+{"DATE":"1/1/15","Used cars percentage increase":"-3%","New cars pct increase":"0%","Avg.Hourly earnings":"1%","Real Median household income":'4%'},
+{"DATE":"2/1/15","Used cars percentage increase":"-1%","New cars pct increase":"0%","Avg.Hourly earnings":"1%","Real Median household income":null},
+{"DATE":"3/1/15","Used cars percentage increase":"-1%","New cars pct increase":"0%","Avg.Hourly earnings":"2%","Real Median household income":null},
+{"DATE":"4/1/15","Used cars percentage increase":"0%","New cars pct increase":"1%","Avg.Hourly earnings":"2%","Real Median household income":null},
+{"DATE":"5/1/15","Used cars percentage increase":"0%","New cars pct increase":"1%","Avg.Hourly earnings":"2%","Real Median household income":null},
+{"DATE":"6/1/15","Used cars percentage increase":"0%","New cars pct increase":"1%","Avg.Hourly earnings":"2%","Real Median household income":null},
+{"DATE":"7/1/15","Used cars percentage increase":"0%","New cars pct increase":"1%","Avg.Hourly earnings":"2%","Real Median household income":null},
+{"DATE":"8/1/15","Used cars percentage increase":"0%","New cars pct increase":"1%","Avg.Hourly earnings":"3%","Real Median household income":null},
+{"DATE":"9/1/15","Used cars percentage increase":"-1%","New cars pct increase":"1%","Avg.Hourly earnings":"3%","Real Median household income":null},
+{"DATE":"10/1/15","Used cars percentage increase":"-2%","New cars pct increase":"1%","Avg.Hourly earnings":"3%","Real Median household income":null},
+{"DATE":"11/1/15","Used cars percentage increase":"-3%","New cars pct increase":"1%","Avg.Hourly earnings":"3%","Real Median household income":null},
+{"DATE":"12/1/15","Used cars percentage increase":"-3%","New cars pct increase":"1%","Avg.Hourly earnings":"3%","Real Median household income":null},
+{"DATE":"1/1/16","Used cars percentage increase":"-3%","New cars pct increase":"0%","Avg.Hourly earnings":"4%","Real Median household income":'7%'},
+{"DATE":"2/1/16","Used cars percentage increase":"-3%","New cars pct increase":"1%","Avg.Hourly earnings":"4%","Real Median household income":null},
+{"DATE":"3/1/16","Used cars percentage increase":"-4%","New cars pct increase":"1%","Avg.Hourly earnings":"4%","Real Median household income":null},
+{"DATE":"4/1/16","Used cars percentage increase":"-2%","New cars pct increase":"1%","Avg.Hourly earnings":"4%","Real Median household income":null},
+{"DATE":"5/1/16","Used cars percentage increase":"-2%","New cars pct increase":"1%","Avg.Hourly earnings":"5%","Real Median household income":null},
+{"DATE":"6/1/16","Used cars percentage increase":"-3%","New cars pct increase":"1%","Avg.Hourly earnings":"5%","Real Median household income":null},
+{"DATE":"7/1/16","Used cars percentage increase":"-4%","New cars pct increase":"1%","Avg.Hourly earnings":"5%","Real Median household income":null},
+{"DATE":"8/1/16","Used cars percentage increase":"-4%","New cars pct increase":"1%","Avg.Hourly earnings":"5%","Real Median household income":null},
+{"DATE":"9/1/16","Used cars percentage increase":"-3%","New cars pct increase":"1%","Avg.Hourly earnings":"5%","Real Median household income":null},
+{"DATE":"10/1/16","Used cars percentage increase":"-5%","New cars pct increase":"1%","Avg.Hourly earnings":"6%","Real Median household income":null},
+{"DATE":"11/1/16","Used cars percentage increase":"-6%","New cars pct increase":"1%","Avg.Hourly earnings":"6%","Real Median household income":null},
+{"DATE":"12/1/16","Used cars percentage increase":"-6%","New cars pct increase":"1%","Avg.Hourly earnings":"6%","Real Median household income":null},
+{"DATE":"1/1/17","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"6%","Real Median household income":'8%'},
+{"DATE":"2/1/17","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"7%","Real Median household income":null},
+{"DATE":"3/1/17","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"7%","Real Median household income":null},
+{"DATE":"4/1/17","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"7%","Real Median household income":null},
+{"DATE":"5/1/17","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"7%","Real Median household income":null},
+{"DATE":"6/1/17","Used cars percentage increase":"-8%","New cars pct increase":"1%","Avg.Hourly earnings":"7%","Real Median household income":null},
+{"DATE":"7/1/17","Used cars percentage increase":"-8%","New cars pct increase":"0%","Avg.Hourly earnings":"8%","Real Median household income":null},
+{"DATE":"8/1/17","Used cars percentage increase":"-8%","New cars pct increase":"0%","Avg.Hourly earnings":"8%","Real Median household income":null},
+{"DATE":"9/1/17","Used cars percentage increase":"-9%","New cars pct increase":"0%","Avg.Hourly earnings":"8%","Real Median household income":null},
+{"DATE":"10/1/17","Used cars percentage increase":"-7%","New cars pct increase":"0%","Avg.Hourly earnings":"8%","Real Median household income":null},
+{"DATE":"11/1/17","Used cars percentage increase":"-7%","New cars pct increase":"0%","Avg.Hourly earnings":"8%","Real Median household income":null},
+{"DATE":"12/1/17","Used cars percentage increase":"-7%","New cars pct increase":"0%","Avg.Hourly earnings":"9%","Real Median household income":null},
+{"DATE":"1/1/18","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"9%","Real Median household income":'9%'},
+{"DATE":"2/1/18","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"9%","Real Median household income":null},
+{"DATE":"3/1/18","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"10%","Real Median household income":null},
+{"DATE":"4/1/18","Used cars percentage increase":"-7%","New cars pct increase":"0%","Avg.Hourly earnings":"10%","Real Median household income":null},
+{"DATE":"5/1/18","Used cars percentage increase":"-7%","New cars pct increase":"0%","Avg.Hourly earnings":"10%","Real Median household income":null},
+{"DATE":"6/1/18","Used cars percentage increase":"-7%","New cars pct increase":"0%","Avg.Hourly earnings":"11%","Real Median household income":null},
+{"DATE":"7/1/18","Used cars percentage increase":"-8%","New cars pct increase":"0%","Avg.Hourly earnings":"11%","Real Median household income":null},
+{"DATE":"8/1/18","Used cars percentage increase":"-7%","New cars pct increase":"0%","Avg.Hourly earnings":"11%","Real Median household income":null},
+{"DATE":"9/1/18","Used cars percentage increase":"-8%","New cars pct increase":"0%","Avg.Hourly earnings":"12%","Real Median household income":null},
+{"DATE":"10/1/18","Used cars percentage increase":"-7%","New cars pct increase":"0%","Avg.Hourly earnings":"12%","Real Median household income":null},
+{"DATE":"11/1/18","Used cars percentage increase":"-6%","New cars pct increase":"0%","Avg.Hourly earnings":"12%","Real Median household income":null},
+{"DATE":"12/1/18","Used cars percentage increase":"-6%","New cars pct increase":"1%","Avg.Hourly earnings":"13%","Real Median household income":null},
+{"DATE":"1/1/19","Used cars percentage increase":"-5%","New cars pct increase":"1%","Avg.Hourly earnings":"13%","Real Median household income":'17%'},
+{"DATE":"2/1/19","Used cars percentage increase":"-6%","New cars pct increase":"1%","Avg.Hourly earnings":"13%","Real Median household income":null},
+{"DATE":"3/1/19","Used cars percentage increase":"-7%","New cars pct increase":"2%","Avg.Hourly earnings":"14%","Real Median household income":null},
+{"DATE":"4/1/19","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"13%","Real Median household income":null},
+{"DATE":"5/1/19","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"14%","Real Median household income":null},
+{"DATE":"6/1/19","Used cars percentage increase":"-6%","New cars pct increase":"0%","Avg.Hourly earnings":"14%","Real Median household income":null},
+{"DATE":"7/1/19","Used cars percentage increase":"-6%","New cars pct increase":"0%","Avg.Hourly earnings":"15%","Real Median household income":null},
+{"DATE":"8/1/19","Used cars percentage increase":"-5%","New cars pct increase":"0%","Avg.Hourly earnings":"15%","Real Median household income":null},
+{"DATE":"9/1/19","Used cars percentage increase":"-6%","New cars pct increase":"0%","Avg.Hourly earnings":"15%","Real Median household income":null},
+{"DATE":"10/1/19","Used cars percentage increase":"-6%","New cars pct increase":"0%","Avg.Hourly earnings":"15%","Real Median household income":null},
+{"DATE":"11/1/19","Used cars percentage increase":"-6%","New cars pct increase":"0%","Avg.Hourly earnings":"16%","Real Median household income":null},
+{"DATE":"12/1/19","Used cars percentage increase":"-6%","New cars pct increase":"1%","Avg.Hourly earnings":"16%","Real Median household income":null},
+{"DATE":"1/1/20","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"16%","Real Median household income":'14%'},
+{"DATE":"2/1/20","Used cars percentage increase":"-7%","New cars pct increase":"2%","Avg.Hourly earnings":"17%","Real Median household income":null},
+{"DATE":"3/1/20","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"18%","Real Median household income":null},
+{"DATE":"4/1/20","Used cars percentage increase":"-7%","New cars pct increase":"1%","Avg.Hourly earnings":"23%","Real Median household income":null},
+{"DATE":"5/1/20","Used cars percentage increase":"-7%","New cars pct increase":"0%","Avg.Hourly earnings":"22%","Real Median household income":null},
+{"DATE":"6/1/20","Used cars percentage increase":"-9%","New cars pct increase":"0%","Avg.Hourly earnings":"20%","Real Median household income":null},
+{"DATE":"7/1/20","Used cars percentage increase":"-7%","New cars pct increase":"0%","Avg.Hourly earnings":"20%","Real Median household income":null},
+{"DATE":"8/1/20","Used cars percentage increase":"-2%","New cars pct increase":"0%","Avg.Hourly earnings":"21%","Real Median household income":null},
+{"DATE":"9/1/20","Used cars percentage increase":"4%","New cars pct increase":"1%","Avg.Hourly earnings":"21%","Real Median household income":null},
+{"DATE":"10/1/20","Used cars percentage increase":"5%","New cars pct increase":"1%","Avg.Hourly earnings":"21%","Real Median household income":null},
+{"DATE":"11/1/20","Used cars percentage increase":"4%","New cars pct increase":"2%","Avg.Hourly earnings":"21%","Real Median household income":null},
+{"DATE":"12/1/20","Used cars percentage increase":"3%","New cars pct increase":"3%","Avg.Hourly earnings":"22%","Real Median household income":null},
+{"DATE":"1/1/21","Used cars percentage increase":"2%","New cars pct increase":"3%","Avg.Hourly earnings":"22%","Real Median household income":'13%'},
+{"DATE":"2/1/21","Used cars percentage increase":"2%","New cars pct increase":"3%","Avg.Hourly earnings":"23%","Real Median household income":null},
+{"DATE":"3/1/21","Used cars percentage increase":"2%","New cars pct increase":"3%","Avg.Hourly earnings":"23%","Real Median household income":null},
+{"DATE":"4/1/21","Used cars percentage increase":"12%","New cars pct increase":"3%","Avg.Hourly earnings":"23%","Real Median household income":null},
+{"DATE":"5/1/21","Used cars percentage increase":"21%","New cars pct increase":"4%","Avg.Hourly earnings":"24%","Real Median household income":null},
+{"DATE":"6/1/21","Used cars percentage increase":"32%","New cars pct increase":"5%","Avg.Hourly earnings":"25%","Real Median household income":null},
+{"DATE":"7/1/21","Used cars percentage increase":"31%","New cars pct increase":"7%","Avg.Hourly earnings":"25%","Real Median household income":null},
+{"DATE":"8/1/21","Used cars percentage increase":"29%","New cars pct increase":"8%","Avg.Hourly earnings":"26%","Real Median household income":null},
+{"DATE":"9/1/21","Used cars percentage increase":"29%","New cars pct increase":"9%","Avg.Hourly earnings":"27%","Real Median household income":null},
+{"DATE":"10/1/21","Used cars percentage increase":"33%","New cars pct increase":"11%","Avg.Hourly earnings":"27%","Real Median household income":null},
+{"DATE":"11/1/21","Used cars percentage increase":"37%","New cars pct increase":"13%","Avg.Hourly earnings":"28%","Real Median household income":null},
+{"DATE":"12/1/21","Used cars percentage increase":"42%","New cars pct increase":"15%","Avg.Hourly earnings":"28%","Real Median household income":null},
+{"DATE":"1/1/22","Used cars percentage increase":"44%","New cars pct increase":"15%","Avg.Hourly earnings":"29%","Real Median household income":null},
+{"DATE":"2/1/22","Used cars percentage increase":"44%","New cars pct increase":"15%","Avg.Hourly earnings":"29%","Real Median household income":null},
+{"DATE":"3/1/22","Used cars percentage increase":"38%","New cars pct increase":"16%","Avg.Hourly earnings":"30%","Real Median household income":null},
+{"DATE":"4/1/22","Used cars percentage increase":"37%","New cars pct increase":"16%","Avg.Hourly earnings":"31%","Real Median household income":null},
+{"DATE":"5/1/22","Used cars percentage increase":"40%","New cars pct increase":"17%","Avg.Hourly earnings":"31%","Real Median household income":null},
+{"DATE":"6/1/22","Used cars percentage increase":"41%","New cars pct increase":"17%","Avg.Hourly earnings":"32%","Real Median household income":null},
+{"DATE":"7/1/22","Used cars percentage increase":"40%","New cars pct increase":"18%","Avg.Hourly earnings":"32%","Real Median household income":null},
+{"DATE":"8/1/22","Used cars percentage increase":"39%","New cars pct increase":"19%","Avg.Hourly earnings":"33%","Real Median household income":null},
+{"DATE":"9/1/22","Used cars percentage increase":"38%","New cars pct increase":"20%","Avg.Hourly earnings":"33%","Real Median household income":null},
+{"DATE":"10/1/22","Used cars percentage increase":"35%","New cars pct increase":"20%","Avg.Hourly earnings":"34%","Real Median household income":null},
+{"DATE":"11/1/22","Used cars percentage increase":"33%","New cars pct increase":"21%","Avg.Hourly earnings":"34%","Real Median household income":null},
+{"DATE":"12/1/22","Used cars percentage increase":"30%","New cars pct increase":"22%","Avg.Hourly earnings":"35%","Real Median household income":null},
+{"DATE":"1/1/23","Used cars percentage increase":"28%","New cars pct increase":"22%","Avg.Hourly earnings":"35%","Real Median household income":null},
+{"DATE":"2/1/23","Used cars percentage increase":"24%","New cars pct increase":"22%","Avg.Hourly earnings":"35%","Real Median household income":null}]
+
+// Set the dimensions and margins of the graph
+var margin = {top: 20, right: 30, bottom: 30, left: 60},
+    width = 600 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// Parse the date / time
+var parseTime = d3.timeParse("%m/%d/%y");
+
+// Set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// Define the line
+var line1 = d3.line()
+    .x(function(d) { return x(d.DATE); })
+    .y(function(d) { return y(d["Used cars percentage increase"]); });
+
+var line2 = d3.line()
+    .x(function(d) { return x(d.DATE); })
+    .y(function(d) { return y(d["New cars pct increase"]); });
+
+var line3 = d3.line()
+    .x(function(d) { return x(d.DATE); })
+    .y(function(d) { return y(d["Avg.Hourly earnings"]); });
+
+var line4 = d3.line()
+    .x(function(d) { return x(d.DATE); })
+    .y(function(d) { return y(d["Real Median household income"]); });
+
+// Append the svg object to the body of the page
+var svg = d3.select("#my_graph")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Format the data
+myData.forEach(function(d) {
+  d.DATE = parseTime(d.DATE);
+  d["Used cars percentage increase"] = +d["Used cars percentage increase"];
+  d["New cars pct increase"] = +d["New cars pct increase"];
+  d["Avg.Hourly earnings"] = +d["Avg.Hourly earnings"];
+  d["Real Median household income"] = +d["Real Median household income"];
+});
+
+// Scale the range of the data
+x.domain(d3.extent(myData, function(d) { return d.DATE; }));
+y.domain([-2, 2]);
+
+// Add the X Axis
+svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+// Add the Y Axis
+svg.append("g")
+    .call(d3.axisLeft(y));
+
+// Add the lines
+svg.append("path")
+  .data([myData])
+  .attr("class", "line")
+  .attr("d", line1);
+
+svg.append("path")
+  .data([myData])
+  .attr("class", "line")
+  .style("stroke", "red")
+  .attr("d", line2);
+
+svg.append("path")
+  .data([myData])
+  .attr("class", "line")
+  .style("stroke", "green")
+  .attr("d", line3);
+
+svg.append("path")
+  .data([myData])
+  .attr("class", "line")
+  .style("stroke", "purple")
+  .attr("d", line4);
+
+
+  
